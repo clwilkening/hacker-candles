6c63767cf57ed88c2a4e2d30674c9520c7c68cc0
Chris Wilkening <clw13clw13@gmail.com>
2017-02-23 11:19:40 -0500

    add cart functionality. delete/price update/quant change

--- client/src/App.js
+++ client/src/App.js
@@ -16,7 +16,8 @@ class App extends Component {
 
     this.state = {
       volume: "8oz",
-      cart: {}
+      cart: {},
+      totalPrice: 0,
     };
   }
     componentDidMount() {
@@ -35,10 +36,70 @@ class App extends Component {
   }
 
   addToCart(key) {
-    console.log(`HERE DA KEY +:+ ${key}`)
-    const order = {...this.state.order}
-    order[key] = order[key] + 1 || 1
-    this.setState({ cart: order})
+    // console.log(`HERE DA KEY +:+ ${key}`)
+    const cart = {...this.state.cart};
+    cart[key] = cart[key] + 1 || 1
+    let order = {...this.state.inventoryObject};
+    let price = this.state.totalPrice;
+    price = price + order[key].price
+    this.setState({
+      cart: cart,
+      totalPrice: price,
+    })
+  }
+
+  removeCandle = (key) => {
+    console.log(`key = ${key}`)
+    let cart = {...this.state.cart};
+    console.log(`cart = ${JSON.stringify(cart)}`)
+    let candlePrice = this.state.inventoryObject[key].price;
+    console.log(`candle price = ${candlePrice}`)
+    console.log(`cart[key] = ${cart[key]}`)
+    let priceToSubtract = candlePrice * cart[key];
+    let totalPrice = this.state.totalPrice;
+    console.log(`TotalPrice = ${totalPrice}`)
+    let newPrice = totalPrice - priceToSubtract
+    console.log(`new Price = ${newPrice}`)
+    delete cart[key];
+    this.setState({
+      cart,
+      totalPrice: newPrice,
+    })
+  }
+
+  updateCart = (key, value) => {
+    let cart = {...this.state.cart};
+    cart[key] = value;
+    // console.log(cart[key])
+    // console.log(`after ${JSON.stringify(cart)}`)
+    let totalPrice = this.state.totalPrice;
+    console.log(totalPrice)
+    let candlePrice = this.state.inventoryObject[key].price;
+    console.log(candlePrice)
+    //loop through cart, grab values
+    let cartItems =[];
+      for (var key in cart) {
+        // console.log(`My key loopty loop === ${key}`)
+        var obj = cart[key]
+        // console.log(`My object === ${obj}`)
+        if (isNaN(obj)){
+          cartItems.pop(obj)
+        } else {
+        cartItems.push(obj)
+        // console.log(`Twas a number === ${cartItems}`)
+        }
+      }
+    let getSum = (total, num) => {
+      return total + num
+    }
+    let newCartItems = cartItems.reduce(getSum)
+    // console.log(`new cart items = ${newCartItems}`)
+    let newTotal = candlePrice * newCartItems;
+    console.log(newTotal)
+    this.setState({
+      cart,
+      totalPrice: newTotal,
+    });
   }
 
   render() {
@@ -72,7 +133,13 @@ class App extends Component {
               <About inventoryObject={this.state.inventoryObject} />
             )} />
             <Route exact path="/cart" render={() => (
-              <Cart inventoryObject={this.state.inventoryObject} />
+              <Cart
+              inventoryObject={this.state.inventoryObject}
+              cart={this.state.cart}
+              totalPrice={this.state.totalPrice}
+              updateCart={this.updateCart}
+              removeCandle={this.removeCandle}
+              />
             )} />
             <Route component={NotFound} />
           </Switch>
--- client/src/components/Cart.js
+++ client/src/components/Cart.js
@@ -4,13 +4,59 @@ class Cart extends Component {
   constructor(props) {
     super(props);
 
+    this.renderCart = this.renderCart.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+
     // this.state = {};
   }
 
+  handleChange(e, key) {
+    const item = this.props.cart[key]
+    const value = parseInt(e.target.value, 10);
+    this.props.updateCart(key, value);
+  }
+
+  renderCart() {
+    const cart = {...this.props.cart}
+    const candle = {...this.props.inventoryObject};
+    // console.log(candle);
+    var cartElements = [];
+      for(let key in cart) {
+        // console.log(key);
+        let item = cart[key]
+        // console.log(`ITEM FROM LOOP ++++ ${item}`)
+        // console.log(`here is candle loop ${JSON.stringify(candle)}`)
+        // console.log(candle[key].name)
+        cartElements.push(
+          <div className="cart-item" key={key}>
+          <h4>{candle[key].name}</h4><button onClick={() => this.props.removeCandle(key)}>X</button>
+          <img src="http://placehold.it/250x250"></img>
+          <p>price/candle: ${candle[key].price}</p>
+          <p>scent: {candle[key].scent}</p>
+          <p>quantity:</p> <input type="number" min="0" max="10" defaultValue={item} onChange={(e) => this.handleChange(e, key)}></input>
+
+          </div>
+        )
+      }
+      // console.log(cartElements)
+      return (
+        <div>
+        {cartElements}
+        </div>
+      )
+  }
+
   render() {
+    let {cart, totalPrice, inventoryObject} = this.props;
+    // console.log('cart = ' + JSON.stringify(cart) )
     return(
+      JSON.stringify(cart) === "{}" ?
       <div>
-      <h1> here is the cart </h1>
+      <h1>No Candles in Cart</h1>
+      </div>
+      :
+      <div>
+      {this.renderCart()}
       </div>
     );
   };
